521f670ca43390e4943b8edb4a4067893f62c617
morisum <moris@r.sannet.ne.jp>
2026-07-25 10:54:21 +0900

    Initial Commit

---
+++ MyTestView/MyTestViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestViewApp.swift
+//  MyTestView
+//
+//  Created by MsMacM on 2026/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTestViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyTestViewTests/MyTestViewTests.swift
@@ -0,0 +1,19 @@
+//
+//  MyTestViewTests.swift
+//  MyTestViewTests
+//
+//  Created by MsMacM on 2026/07/25.
+//
+
+import Testing
+@testable import MyTestView
+
+struct MyTestViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
