134bb28a6653dc0b8da94a6afdf6b7c3686b3e37
morisum <moris@r.sannet.ne.jp>
2026-07-27 10:20:53 +0900

    AppをContentViewからMainScreenに変更した。

--- MyTestView/MyTestViewApp.swift
+++ MyTestView/MyTestViewApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MyTestViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            MainScreen()
         }
     }
 }
